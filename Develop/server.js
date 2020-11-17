2bef0649fb28571bed081171733fe48ee3c401a6
Joel Estrada <jre.estrada@gmail.com>
2020-11-17 06:23:05 -0800

    added functionality to app.delete

--- Develop/server.js
+++ Develop/server.js
@@ -52,9 +52,18 @@ app.post("/api/notes", (req, res) => {
 
 // DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 app.delete("/api/notes/:id", (req, res) => {
-    let idParameter = req.params.id;
-
-    console.log("test delete" + idParameter);
+    let index = 0;
+    for (let i = 0; i < allNotes.length; i++) {
+        if (allNotes[i].id === req.params.id) {
+            index = i;
+        }
+    }
+    allNotes.splice(index, 1);
+    console.log("test delete" + req.params.id);
+    fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes), err => {
+        if (err) throw err;
+        res.end();
+    });
 });
 
 app.get("*", (req, res) => {
